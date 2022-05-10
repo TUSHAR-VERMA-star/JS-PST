84effbbff4dfe9fb3ae392099d7861dfdc382c8e
TUSHAR-VERMA-star <tusharverma1510@gmail.com>
2022-05-10 23:45:37 +0530

    Third Comiit by Tushar

--- Tushar/index.js
+++ Tushar/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+let count=0;
+let save=false;
+
+let s = " Tushar";
+let value = document.getElementById("welcome-ele").innerHTML;
+let tobeprinted = value + s;
+document.getElementById("welcome-ele").innerHTML = tobeprinted;
+
+function increment()
+{
+  if(save)
+  {
+    document.getElementById("print-kro").innerHTML = count;
+  }
+  else
+  {
+    count+=1;
+    console.log(count);
+    document.getElementById("print-kro").innerHTML = count;
+  }
+   
+}
+function savef()
+{
+  save=true;
+  
+}
