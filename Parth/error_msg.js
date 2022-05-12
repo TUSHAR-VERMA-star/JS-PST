bd6ab0cbfad0118f4b1cfc7a31e00d97ba784cda
iparth36i <iparth36@gmail.com>
2022-05-12 19:23:33 +0530

    Parth Gandhi ko sabh aata hai

---
+++ Parth/error_msg.js
@@ -0,0 +1,4 @@
+function purchase()
+{
+    alert("smriti not available");
+}
--- Parth/index.js
+++ Parth/index.js
@@ -1,1 +1,29 @@
-console.log("Hello World Parth");
+let count=0;
+let save=false;
+
+let s = " Tushar";
+let value = document.getElementById("welcome-ele").innerHTML;
+let tobeprinted = value + s;
+document.getElementById("welcome-ele").innerHTML = tobeprinted;
+
+function increment()
+{
+    count+=1;
+    console.log(count);
+    document.getElementById("print-kro").innerHTML = count;
+  
+   
+}
+
+function savef()
+{
+  let str = count + " -    ";
+  
+  document.getElementById("saves").innerText += str;
+}
+
+function setZero()
+{
+  count = 0;
+  document.getElementById("print-kro").innerHTML = count;
+} 
