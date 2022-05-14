704fa34262c363968528530d70456c99d0d304e7
TUSHAR-VERMA-star <tusharverma1510@gmail.com>
2022-05-14 12:28:43 +0530

    Tushar Commit

--- Tushar/New project black/index.js
+++ Tushar/New project black/index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 5
+let arr = [firstCard, secondCard]
 let iscard=false;
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,29 +10,24 @@ let messageEl = document.getElementById("message-el")
 let cardd = document.getElementById("card")
 let summ = document.getElementById("sum")
 let card = 0;
-// 2. Store the sum paragraph in a variable called sumEl
 
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
-    
-    
     summ.innerHTML = "ANS = " + sum;
-    if(iscard)
+    let ans = " "
+    for(let i=0; i<arr.length; i++)
     {
-        cardd.innerHTML = "Card = " + firstCard + " " + secondCard +" "+ card;
+        ans+=arr[i]+" ";
     }
-    else
-    {
-        cardd.innerHTML = "Card = " + firstCard + " " + secondCard;
-    }
+    cardd.innerHTML = "Card = " + ans;
     
-
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
+        document.getElementById("newcard").textContent="END GAME"
         message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
+        document.getElementById("newcard").textContent="END GAME"
         message = "You're out of the game!"
         isAlive = false
     }
@@ -39,8 +35,16 @@ function startGame() {
 }
 function newGame()
 {
-    card = Math.floor(Math.random() *(11 - 2)+ 2)
-    iscard=true;
-    sum+=card;
-    startGame(); 
+    if(sum<=21)
+    {
+        card = Math.floor(Math.random() *(11 - 2)+ 2)
+        arr.push(card);
+        iscard=true;
+        sum+=card;
+        startGame(); 
+    }
+    else{
+        document.getElementById("newcard").textContent="END GAME"
+    }
+    
 }
