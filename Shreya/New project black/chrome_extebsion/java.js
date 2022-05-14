1b5fcd149c273493bd3f82799a85108adc38cdf1
Shreya20-web <shreyabhardwaj102@gmail.com>
2022-05-14 21:53:31 +0530

    digital clock

---
+++ Shreya/New project black/chrome_extebsion/java.js
@@ -0,0 +1,10 @@
+// function clickk(){
+//     document.getElementById("msg").textContent = "button clicked!!"
+    
+// }
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("button clicked from event listener!!")
+}) 
