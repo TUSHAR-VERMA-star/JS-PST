ca18bab3fc0153c4e528c3d07e25ee8f184f4457
Shreya20-web <shreyabhardwaj102@gmail.com>
2022-05-27 18:22:28 +0530

    movie seat booking

--- Shreya/New project black/Movie_sets_booking/movie.js
+++ Shreya/New project black/Movie_sets_booking/movie.js
@@ -1,15 +1,28 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
 
-let count = 1
-function calculate(val) {
-    document.getElementById(val).textContent = count
-    document.getElementById(val).style.backgroundColor = "cyan";
-    document.getElementById("selected-btn").style.backgroundColor = "cyan";
+let ticketPrice = +movieSelect.value;
 
-    const collection = document.getElementsByClassName("occupied-btn");
-    for (let i = 0; i < collection.length; i++) {
-        collection[i].style.backgroundColor = "pink";
-    }
-    
-    document.getElementById("price").textContent = "you have selected " + count + "seats and total cost is " + count*10
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
 
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
 }
+
+movieSelect.addEventListener('change', e=>{
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
+});
+
+container.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+});
